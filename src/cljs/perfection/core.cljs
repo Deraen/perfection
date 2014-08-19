82945569456e952ea5cabcf9ac76b1ba1fed6332
Juho Teperi <juho.teperi@iki.fi>
2014-08-20 02:34:50 +0300

    .

--- src/cljs/perfection/core.cljs
+++ src/cljs/perfection/core.cljs
@@ -1,22 +1,24 @@
 (ns perfection.core
-  #_(:require [om.core :as om :include-macros true]
+  (:require [om.core :as om :include-macros true]
             [om-tools.core :refer-macros [defcomponent]]
             [sablono.core :as html :refer-macros [html]]))
 
-(.log js/console "Hello world 2!")
+(enable-console-print!)
+
+(println "Hello world 2!")
 
 (defn add [a b]
   (+ a b))
 
-; (defcomponent widget [data owner]
-;   (will-mount [_]
-;     (om/set-state! owner :n (:init data)))
-;   (render-state [_ {:keys [n]}]
-;     (html [:div (str "Hello world: " n)
-;            [:ul (for [i (range 1 10)]
-;                   [:li [:button
-;                         {:on-click #(om/set-state! owner :n i)
-;                          :class ["btn" "btn-default" (if (= i n) "btn-success")]}
-;                         (str "Foobar" i)]])]])))
+(defcomponent widget [data owner]
+  (will-mount [_]
+    (om/set-state! owner :n (:init data)))
+  (render-state [_ {:keys [n]}]
+    (html [:div (str "Hello world: " n)
+           [:ul (for [i (range 1 10)]
+                  [:li [:button
+                        {:on-click #(om/set-state! owner :n i)
+                         :class ["btn" "btn-default" (if (= i n) "btn-success")]}
+                        (str "Foobar" i)]])]])))
 
-; (om/root widget {:init 5} {:target js/document.body})
+(om/root widget {:init 5} {:target js/document.body})
