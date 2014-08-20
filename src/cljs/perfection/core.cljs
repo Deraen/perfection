a36734438037b21ec1fcabb9824d7396d14c2233
Juho Teperi <juho.teperi@iki.fi>
2014-08-20 22:22:30 +0300

    pizza shop

--- src/cljs/perfection/core.cljs
+++ src/cljs/perfection/core.cljs
@@ -7,18 +7,60 @@
 
 (println "Hello world 2!")
 
-(defn add [a b]
-  (+ a b))
+(defonce app-state (atom {:cart {"Alla Pollo" 5}
+                          :pizzas [{:name "Alla Pollo"
+                                    :price    8.30
+                                    :toppings {:onion 2
+                                               :ham   3}}
+                                   {:name "Americana"
+                                    :price    7.90
+                                    :toppings {:pineaple    2
+                                               :ham         3
+                                               :blue-cheese 1}}
+                                   {:name "Margherita"
+                                    :price    7.20
+                                    :toppings {:mozarella 4
+                                               :tomato    3}}]}))
 
-(defcomponent widget [data owner]
+(defcomponent pizza-item [{:keys [name price toppings]} owner]
+  (init-state [_]
+    {:open false})
+  (render-state [_ data]
+    (html [:div
+           [:h3
+            [:button {:on-click #(om/update-state! owner :open not)} ">"]
+            (str name ", " price "€")
+            [:button {:on-click (fn [] (swap! app-state update-in [:cart name] dec))} "-"]
+            [:button {:on-click (fn [] (swap! app-state update-in [:cart name] inc))} "+"]]
+           (if (:open data)
+             [:ul (for [[topping n] toppings]
+                    [:li (str topping " " n " (?)")])])])))
+
+(defcomponent pizza-list [data owner]
   (render-state [_ _]
     (println "rendering")
-    (html [:div (str "Hello world: " (:n data))
-           [:ul (for [i (range 1 10)]
-                  [:li [:button
-                        {:on-click #(om/update! data :n i)
-                         :class ["btn" "btn-default" (if (= i (:n data)) "btn-success")]}
-                        (str "Foobar" i)]])]])))
+    (html [:div (om/build-all pizza-item data)])))
 
-(defonce app-state (atom {:n 5}))
-(om/root widget app-state {:target js/document.body})
+(defcomponent shopping-cart [data owner]
+  (render-state [_ _]
+    (html [:div
+           [:ul (for [[name qty] data]
+                  [:li (str name " * " qty)])]
+           [:span (str "Yhteensä: "
+                       (reduce (fn [acc [name qty]]
+                                 (+ acc (->> @app-state
+                                             :pizzas
+                                             (some #(if (= name (:name %)) %))
+                                             :price
+                                             (* qty))))
+                               0
+                               data))]])))
+
+(defcomponent pizza-shop [data owner]
+  (render-state [_ _]
+    (html [:div
+           [:h1 "Pizza shop"]
+           (om/build pizza-list (:pizzas data))
+           (om/build shopping-cart (:cart data))])))
+
+(om/root pizza-shop app-state {:target js/document.body})
